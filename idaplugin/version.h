f76ca8e7291c6f945b5cd26fc450af61b6fb5253
nevergiveupcpp <nevergiveupcxx@gmail.com>
2026-06-21 20:16:27 +0700

    feat: add HCLI support

--- idaplugin/version.h
+++ idaplugin/version.h
@@ -2,7 +2,7 @@
 #define NGU_OMD_VERSION_H
 
 #define PLUGIN_NAME "Oh My Dump"
-#define PLUGIN_VERSION "1.0.14"
+#define PLUGIN_VERSION "1.0.15"
 #define PLUGIN_HOTKEY "Ctrl+Shift+F10"
 
 #endif // NGU_OMD_VERSION_H
